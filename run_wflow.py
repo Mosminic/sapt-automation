41c1337a5559f9739b4c88d1f720fb6e39eefbbc
DaPanda03 <77337331+DaPanda03@users.noreply.github.com>
2023-03-11 17:09:39 -0500

    Create run_wflow.py

---
+++ run_wflow.py
@@ -0,0 +1,20 @@
+from workflow.wflow import *
+from fireworks import LaunchPad
+import pandas as pd
+
+
+# Generate the workflow
+# change the location of the file each time
+
+csv_location = "/scratch/vka247/sapt_automation/files/dimers_test_sapt.csv"
+
+counter = 0
+
+df = pd.read_csv(csv_location)
+for idx, row in df.iterrows():
+    if(counter == 0):
+        counter = counter + 1
+    else:
+        wf = SAPTWorkflow(row["identifier"], row["xyz"], row["label"])
+        lpad = LaunchPad.from_file("my_launchpad.yaml")
+        lpad.add_wf(wf)
